77672e38d366e4878ae886571217652246b08acf
fedono <fedono@163.com>
2020-06-08 21:56:49 +0800

    add dp

--- test/neo.js
+++ test/neo.js
@@ -1,16 +1,29 @@
-let s = "13[a]2[bc]";
+const climbStairs = function(n) {
+    if (n === 1) {
+        return 1;
+    }
 
-let res = s.replace(/(\d+)\[(\w+)\]/g, (a, b, c) => {
-    return c.repeat(b);
-});
-console.log(res);
+    if (n === 2) {
+        return 2;
+    }
 
-const decodeString = function (s) {
-    return s.replace(/(\d+)\[(\w+)\]/g, (a, b, c) => {
-        return c.repeat(b);
-    });
+    return climbStairs(n - 2) + climbStairs(n - 1);
 }
 
-let res1 = decodeString(s);
-console.log(res1);
-console.log(s)
+const climbStairs1 = function(n) {
+    if (n === 1) {
+        return;
+    }
+
+    let dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i -2];
+    }
+    return dp[n];
+}
+
+console.log(climbStairs(15))
+console.log(climbStairs1(15))
