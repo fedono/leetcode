ad892fd1dca8a52d034e8881cee9663e6df6cf48
fedono <fedono@163.com>
2020-06-21 17:44:56 +0800

    add 合并区间

--- test/neo.js
+++ test/neo.js
@@ -1,22 +1,22 @@
-// 给定一个升序整型数组[0,1,2,4,5,7,13,15,16],找出其中连续出现的数字区间，输出为["0->2","4->5","7","13","15->16"]
+const merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
 
-// 找到所有的连续区间，然后输出的时候获取 start / end 就好
-// 其实就是建立一个栈，循环的时候，把第一个加入进去，如果当前这个不是前一个的 +1，那么久添加到需要输出的数组中去
-
-const summaryRanges = function(arr) {
     let res = [];
+    let idx = -1;
 
-    for (let i = 0, len = arr.length; i < len; i++) {
-        if (arr[i] === arr[i - 1] + 1) {
-            let prev = res[res.length - 1].split('->')[0];
-            res[res.length - 1] = `${prev}->${arr[i]}`;
-        }
-        else {
-            res.push(String(arr[i]))
+    for (let interval of intervals) {
+        if (idx == -1 || interval[0] > res[idx][1]) {
+            res.push(interval);
+            idx++;
+        } else {
+            res[idx][1] = Math.max(res[idx][1], interval[1]);
         }
     }
 
     return res;
 }
 
-console.log(summaryRanges([0,1,2,4,5,7,13,15,16]))
+
+let re = merge([[1,3],[2,6],[8,10],[15,18]]);
+console.log(re)
+
